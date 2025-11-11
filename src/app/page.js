fb2f531514ad9b3f9bf1b1ba0592ded338408674
Jose <jpallop0304@g.educaand.es>
2025-11-11 16:52:34 +0100

    feat: add links to client pages

--- src/app/page.js
+++ src/app/page.js
@@ -16,18 +16,31 @@ async function Home() {
       <div className="h-[400px] flex flex-col gap-10 justify-center content-center">
         <div className="flex flex-col items-center">
           <Link href="/productos-db" className="block text-2xl text-blue-400 font-bold">
-            BASE DE DATOS
+            PRODUCTOS (BASE DE DATOS)
           </Link>
           <p>Requisitos previos: <span className="font-bold">Deberás tener una servidor MySQL para la DB local.</span></p>
         </div>
 
         <div className="flex flex-col items-center">
           <Link href="/productos-api" className="block text-2xl text-blue-400 font-bold">
-            API REST
+            PRODUCTOS (API REST)
           </Link>
           <p>Requisitos previos: <span className="font-bold">Deberás tener un servidor JSON para la API local.</span></p>
         </div>
 
+        <div className="flex flex-col items-center">
+          <Link href="/clientes-db" className="block text-2xl text-green-400 font-bold">
+            CLIENTES (BASE DE DATOS)
+          </Link>
+          <p>Requisitos previos: <span className="font-bold">Deberás tener una servidor MySQL para la DB local.</span></p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <Link href="/clientes-api" className="block text-2xl text-green-400 font-bold">
+            CLIENTES (API REST)
+          </Link>
+          <p>Requisitos previos: <span className="font-bold">Deberás tener un servidor JSON para la API local.</span></p>
+        </div>
       </div>
     </section>
   )
