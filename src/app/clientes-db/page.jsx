88433d98a8e0e35c8dde71bb96f7906698591bfb
Jose <jpallop0304@g.educaand.es>
2025-11-11 16:52:12 +0100

    feat: add client pages

---
+++ src/app/clientes-db/page.jsx
@@ -0,0 +1,29 @@
+import { Suspense } from "react";
+import Link from "next/link";
+
+import Fallback from "@/components/fallback";
+import Clientes from "@/components/db-clientes";
+import ClienteNuevo from "@/components/db-cliente-nuevo";
+
+async function ClientesPage({ searchParams }) {
+    let { query } = await searchParams
+    query ??= ''
+
+    return (
+        <section className="min-h-screen max-w-[1024px] mx-auto px-10 py-10">
+            <Link href="/" className="fixed text-4xl p-2 bg-orange-300 rounded-full">🏠</Link>
+
+            <h1 className='py-10 text-3xl text-blue-500 text-center border-b-4 border-b-blue-500'>
+                CLIENTES (BASE DE DATOS)
+            </h1>
+
+            <ClienteNuevo />
+
+            <Suspense fallback={<Fallback>Obteniendo datos ... </Fallback>}>
+                <Clientes query={query} />
+            </Suspense>
+        </section>
+    );
+}
+
+export default ClientesPage;
